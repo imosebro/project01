2a252520a96eacfe5dfb604b426b5e803dd19918
imosebro <imosebro@u.rochester.edu>
2016-11-13 18:10:37 -0500

    Isaac's Part of Project 2
    Improved the Colophon and Footer

--- colophon.php
+++ colophon.php
@@ -9,19 +9,24 @@
 <!-- Portfolio Grid Section -->
     <aside>
         <section id="portfolio">
-            <div class="container">
+            <div class="container text-center">
                 <div class="row">
                     <div class="col-sm-12 portfolio-item">
                         <h2 class="text-center"><center><strong>Colophon</strong></center></h2>
 
-<p>////////////////////////////////////</p>
-<h4>CSC174: Advanced Front End Development</h4>
-<h4>Project 01: Team Website</h4>
-<h4>Team Adonding Poncho (Team 2)</h4>
-<h4>University of Rochester</h4>
-<h4>Professor Kostin</h4>
-<h4>Fall 2016 Semester</h4>
-<p>////////////////////////////////////</p>
+
+<div class="row">
+<a href="#academicInfo" class="btn" data-toggle="collapse"><h4>Academic Info</h4></a>
+
+<div id="academicInfo" class="collapse">
+    <h4>CSC174: Advanced Front End Development</h4>
+    <h4>Project 01: Team Website</h4>
+    <h4>Team Adonding Poncho (Team 2)</h4>
+    <h4>University of Rochester</h4>
+    <h4>Professor Kostin</h4>
+    <h4>Fall 2016 Semester</h4>
+</div>
+</div>
 
 <!--
 /*************
@@ -49,72 +54,95 @@ This team project is a collaborative project for the CSC174: Advanced Front End 
 *************/
 In order to do download the files to your local machine, you will need a GitHub account, GitHub Desktop and internet connection. The go to the repository in where project01 is located(https://github.com/erodri17/project01). Then click ÒClone & downloadÓ button and you should be able to get a copy of the files in your local machine. If you want to create your own repository containing our files, you can go to the repository project02 located (https://github.com/erodri17/project01) and click the ÒforkÓ button. From there on, you are going to be able to make changes to the files from your own repository.
 -->
-<br>
-<br>
-<h4><center><strong>Team Roles and Responsibilities</strong></center></h4>
-<h5>Rayan Ifthikhar- Framework Wrangler </h5>
-<p>Installed the Half Slider framework from Bootstrap and implemented it. Used some content from the bootstrap Justified-Nav template.</p>
-<br>
+<div class="row">
 
-<h5>Elmer Rodriguez- The Architect</h5>
-<p>Created the initial repository that was used to complete the project. In addition, decided to how to split the webpages using PHP includes, which was completed at the end </p>
-<br>
+<a href="#team" class="btn" data-toggle="collapse"><h4>Team Roles</h4></a>
 
-<h5>Joshua Anes- Lead Content Master and Designer</h5>
-<p>Gathered the content and structured it using the Half Slider framework chosen.</p>
-<br>
+<div id="team" class="collapse">
 
-<h5>Ethan Pupkin- CSS Ninja</h5>
-<p>Worked with the Half Slider framework in order to create a custom design that would best represent the website. Also implemented parts of the Justified-Nav CSS in order to make the website look as good as possible.
-</p>
-<br>
+  <h5>Rayan Ifthikhar- Framework Wrangler </h5>
+  <p>Installed the Half Slider framework from Bootstrap and implemented it. Used some content from the bootstrap Justified-Nav template.</p>
 
-<h5>Evan Basta-JavaScript Coder</h5>
-<p>Evan worked implemented JavaScript code for our website. He also assisted in creating the arquitecture of the website and making it more responsive for mobile devices.
-</p>
-<br>
+  <h5>Elmer Rodriguez- The Architect</h5>
+  <p>Created the initial repository that was used to complete the project. In addition, decided to how to split the webpages using PHP includes, which was completed at the end </p>
 
+  <h5>Joshua Anes- Lead Content Master and Designer</h5>
+  <p>Gathered the content and structured it using the Half Slider framework chosen.</p>
 
-<br>
-<h4><center><strong>Student Name - GitHub Account</strong></center></h4>
-<p>* Ethan Pupkin - epupkin21</p>
-<p>* Rayan I - Rayan-I</p>
-<p>* Evan Basta -  Ebasta24</p>
-<p>* Joshua Anes -joshuaanes</p>
-<p>* Elmer Rodriguez - erodri17</p>
-<br>
-<br>
+  <h5>Ethan Pupkin- CSS Ninja</h5>
+  <p>Worked with the Half Slider framework in order to create a custom design that would best represent the website. Also implemented parts of the Justified-Nav CSS in order to make the website look as good as possible.
+  </p>
+
+  <h5>Evan Basta-JavaScript Coder</h5>
+  <p>Evan worked implemented JavaScript code for our website. He also assisted in creating the arquitecture of the website and making it more responsive for mobile devices.
+  </p>
+
+</div>
+</div>
 
 
-<h4><center><strong>Technologies Used</strong></center></h4>
-<ul>
-  <li>MAMP/WAMP</li>
-  <li>Computers</li>
-  <li>Github</li>
-  <li>Github Desktop Application</li>
-  <li>Sublime Text Editor</li>
-  <li>Bootstrap</li>
-  <li>Google Drive</li>
-  <li>PHP</li>
-  <li>HTML</li>
-  <li>CSS</li>
-  <li>JavaScript</li>
-</ul>
-<br>
-<br>
-<h4><center><strong>Works Cited</strong></center></h4>
-<p>C. "Build Software Better, Together." GitHub. N.p., n.d. Web. 02 Nov. 2016.
-"Department of Computer Science." Computer Science Department : Hajim School of Engineering and Applied Sciences. N.p., n.d. Web. 02 Nov. 2016.</p>
-<br>
-<p>Mark Otto, Jacob Thornton, and Bootstrap Contributors. "Bootstrap · The World's Most Popular Mobile-first and Responsive Front-end Framework." Bootstrap · The World's Most Popular Mobile-first and Responsive Front-end Framework. N.p., n.d. Web. 02 Nov. 2016.</p>
-<br>
-<p>MozDevNet. "JavaScript Reference." Mozilla Developer Network. N.p., n.d. Web. 02 Nov. 2016.
-Region, By. "W3C." World Wide Web Consortium (). N.p., n.d. Web. 02 Nov. 2016.</p>
-<br>
-<br>
-<h4><center><strong>Acknowledgments</strong></center></h4>
-<p> We want to thank Professor Robert Kostin for providing Team Adonding Poncho (Team 02) with such a great challenge. Without you this would havenÕt been possible. We want to also thanks the TA's for taking out o their time to help the students of the CSC174 class as well as grade their submissions.</p>
-<br>
+<div class="row">
+
+<a href="#github" class="btn" data-toggle="collapse"><h4>GitHub Accounts</h4></a>
+<div id="github" class="collapse">
+
+  <p>* Ethan Pupkin - epupkin21</p>
+  <p>* Rayan I - Rayan-I</p>
+  <p>* Evan Basta -  Ebasta24</p>
+  <p>* Joshua Anes -joshuaanes</p>
+  <p>* Elmer Rodriguez - erodri17</p>
+</div>
+</div>
+
+
+<div class="row">
+
+<a href="#tech" class="btn" data-toggle="collapse"><h4>Technologies Used</h4></a>
+
+<div id="tech" class="collapse">
+
+    <ul>
+      <li>MAMP/WAMP</li>
+      <li>Computers</li>
+      <li>Github</li>
+      <li>Github Desktop Application</li>
+      <li>Sublime Text Editor</li>
+      <li>Bootstrap</li>
+      <li>Google Drive</li>
+      <li>PHP</li>
+      <li>HTML</li>
+      <li>CSS</li>
+      <li>JavaScript</li>
+    </ul>
+</div>
+</div>
+
+
+<div class="row">
+
+<a href="#works" class="btn" data-toggle="collapse"><h4>Works Cited</h4></a>
+<div id="works" class="collapse">
+
+    <p>C. "Build Software Better, Together." GitHub. N.p., n.d. Web. 02 Nov. 2016.
+    "Department of Computer Science." Computer Science Department : Hajim School of Engineering and Applied Sciences. N.p., n.d. Web. 02 Nov. 2016.</p>
+    <p>Mark Otto, Jacob Thornton, and Bootstrap Contributors. "Bootstrap · The World's Most Popular Mobile-first and Responsive Front-end Framework." Bootstrap · The World's Most Popular Mobile-first and Responsive Front-end Framework. N.p., n.d. Web. 02 Nov. 2016.</p>
+    <p>MozDevNet. "JavaScript Reference." Mozilla Developer Network. N.p., n.d. Web. 02 Nov. 2016.
+    Region, By. "W3C." World Wide Web Consortium (). N.p., n.d. Web. 02 Nov. 2016.</p>
+
+</div>
+</div>
+
+<div class="row">
+
+<a href="#ack" class="btn" data-toggle="collapse"><h4>Acknowledgments</h4></a>
+<div id="ack" class="collapse">
+
+  <p> We want to thank Professor Robert Kostin for providing Team Adonding Poncho (Team 02) with such a great challenge.</p>
+  <p> Without you this would havenÕt been possible. </p>
+   <p> We want to also thanks the TA's for taking out o their time to help the students of the CSC174 class as well as grade their submissions.</p>
+
+</div>
+</div>
 
 
 
