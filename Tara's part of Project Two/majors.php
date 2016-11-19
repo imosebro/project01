a0b68dbe0748f835ac2d533e0b20dc89b7862cab
T Anacker <t.anacker@rochester.edu>
2016-11-19 15:45:18 -0800

    Tara's part of Project Two
    Attempted to clean up the HTML - 1. Added missing </div> tags and fix inconsistent tags  2. Corrected missing alt tags on the images 3. Removed apparently unused tags 4. Attempted to fix the spacing to make it easier to read & to correct 5. Did not remove obsolete <center> tag as that would require CSS to fix - maybe Jack can add the correct CSS to fix? 
    *Hopefully, I have all the divs corrected and it does not mess up any of the styling.

---
+++ Tara's part of Project Two/majors.php
@@ -0,0 +1,117 @@
+    <?php
+        $currentPage = "majors";
+        include "inc/top-part.inc";
+    ?>
+    <!--navbar-->
+    <?php include 'inc/nav.inc';?>
+
+    <div class="row row-offcanvas row-offcanvas-right">
+	  
+        <div class="col-md-12">
+		
+            <h1 class="text-center">Undergraduate Program</h1>
+			
+			<div class="row">
+			
+				<div class="col-md-12">
+				
+					<h2 class="text-left">BS Major Requirements</h2>
+				
+						<p>The BS curriculum is made up of qualifying, core and ﻿advanced courses, as well as the upper-level writing requirement.</p>
+						<p>No more than two core or advanced courses can be completed at other institutions. If transfer courses are taken while in residence, they must be pre-approved by the computer science program undergraduate director. </p>
+						<p>Interested students can also complete the honors research program to be awarded “honors in research,” “high honors in research,” or “highest honors in research.”</p>
+						<p>For complete specifics on the BS major requirements, see the Computer Science Undergraduate Handbook.</p>
+
+					<h2 class= "text-left">Qualifying Courses</h2>
+					
+						<p>CSC 171: The Science of Programming</p>
+						<p>MTH 150: Discrete Mathematics or MTH 150A: Discrete Math Module</p>
+						<p>MTH 161: Calculus I*</p>
+						<p>MTH 162: Calculus II*</p>
+						<p>CSC 172: The Science of Data Structures</p>
+						<p>*Calculus sequences MTH 141-MTH 143, or MTH 171Q and MTH 172Q are also acceptable.</p>
+
+
+					<h2 class= "text-left">Core Courses</h2>
+				
+						<p>CSC 173: Computation and Formal Systems</p>
+						<p>CSC 242: Artificial Intelligence</p>
+						<p>CSC 252: Computer Organization</p>
+						<p>CSC 254: Programming Language Design and Implementation</p>
+						<p>CSC 280: Computer Models and Limitations</p>
+						<p>CSC 282: Design and Analysis of Efficient Algorithms</p>
+						<p>MTH 165: Linear Algebra with Differential Equations **</p>
+						<p>CSC 262: Introduction to Computational Statistics*</p>
+						<p>* This requirement can also be fulfilled with STT/MTH201, STT213 or ECO 230.</p>
+						<p>**Students who take MTH 173Q, or both MTH 163 and MTH 235, may count these options as covering the MTH 165 requirement.</p>
+						<p>Students in 2016-2018 will have the option of doing either CSC 200 or the statistics requirement. Students completing the honors research degree will need to complete CSC 200H and CSC 262 (or substitute).</p>
+
+
+
+					<h2 class= "text-left">Advanced Courses</h2>
+				
+						<p>Students must take three advanced courses in computer science. These courses must be above 200, not included in the core courses, and excluding CSC 390: Supervised Teaching. Students should consult with their faculty adviser or the CS undergraduate coordinator about their advanced course selection.</p>
+						<p>Advanced course specialization is encouraged, but not mandatory. In particular, students are encouraged to select a related set of courses that will prepare them for participation in one of the department’s research projects and/or to complete a senior project.</p>
+						<p>Areas of interest include the following:</p>
+				
+							<ul>
+								<li>Computer Systems</li>
+								<li>Natural Language and Knowledge Representation</li>
+								<li>Machine Learning, Vision and Robotics</li>
+								<li>Theory</li>
+								<li>Human Computer Interaction and Web</li>
+								<li>Data Science</li>
+							</ul>
+
+						<p>In addition to the three advanced CSC courses, a BS candidate must complete one of the following:</p>
+                
+							<ul>
+								<li>CSC 393: Senior Project</li>
+								<li>One additional advanced course in computer science, mathematics or in other computing related disciplines (See section 4.3 in the Computer Science Undergraduate Handbook for more information)</li>
+							</ul>
+
+					<h2 class= "text-left">Advanced Courses</h2>
+				
+						<p>The computer science honors research program requires honors-level coursework and a senior research thesis.</p>
+						<p>There are three degrees of honors awarded:</p>
+						
+							<ul>
+								<li>Honors in research</li>
+								<li>High honors in research</li>
+								<li>Highest honors in research</li>
+							</ul>
+							
+						<p>The degree of honor awarded is determined by the computer science faculty in consultation with the student's honors committee. The quality of the thesis, its presentation, and other CS research and publications throughout the student's undergraduate career are all considered.</p>
+						<br>
+						<p><strong>Honor Requirements</strong></p>
+						<p>CSC 200H: Undergrad Problem Seminar Honors</p>
+						<p>CSC 395H: Honors Senior Thesis</p>
+						<p>CSC 391H: Independent Study-Honors or 393H: Senior Project-Honors</p>
+						<p>Senior Research Thesis</p>
+						<p>For information about the honor program see section 4.4 in the Computer Science Undergraduate Handbook.</p>
+
+					<h2 class ="text-left">Upper-Level Writing Requirement for Computer Science Majors</h2>
+				
+						<p>Every computer science major must develop, in consultation with his or her faculty adviser, a plan that includes two upper-level writing experiences.</p>
+						<p>Each experience must generate at least 25 pages of expository prose, with substantial feedback on content and form, and revision of the work. The 25 pages may be in the form of a single paper or a series of smaller papers.</p>
+						<p>Acceptable writing experiences include the following:</p>
+				
+							<ul>
+								<li>WRT 273: Communicating Your Professional Identity (2 credits) – taken junior year</li>
+								<li>"W" Courses in other departments with advisor approval</li>
+								<li>Designated courses in computer science</li>
+								<li>Creation of a research paper or published technical report</li>
+							</ul>
+							
+						<p>Students should review section 6 in the Computer Science Undergraduate Handbook and consult their advisor for more information.</p>
+
+				</div><!--/.col-xs-6.col-lg-4-->
+		 
+			</div><!--/row-->
+			
+		</div><!--/.col-xs-12.col-sm-9-->
+		
+	</div> <!-- guess this is .row row-offcanvas row-offcanvas-right -->	
+
+    <!--Footer & Javascript-->
+    <?php include "inc/footer.inc"; ?>
